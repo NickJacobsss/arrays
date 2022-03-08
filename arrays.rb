2decb9ddffcdbb6982c8d7a02f2d39b53dd1ce76
Nick Jacobs <nickjacobsatc@gmail.com>
2022-03-08 17:43:07 -0600

    Added ruby commands

--- arrays.rb
+++ arrays.rb
@@ -2,3 +2,18 @@ foods = ["pizza","chicken","steak","candy"]
 cost = [1,2,3,4]
 tax_cost = [1.2,2.2,3.2,4.2]
 amount_paid = [true,false,true,false]
+
+puts foods[1]
+# will output chicken
+
+puts cost[2]
+# will output the number 3
+
+puts tax_cost[0]
+# will output the float 1.2
+
+puts amount_paid[0]
+# will output the boolean true
+
+foods.push("cheezit")
+# will add the string cheezit to the end of the list of foods
